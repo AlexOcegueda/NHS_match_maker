c4a9bed0b911c700b45b786062c3adf2d649a1f2
Alex Ocegueda <alexocegueda59@gmail.com>
2023-07-01 22:38:58 -0500

    inital stages with file setup

---
+++ src/pages/app.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Gatsby App!</h1>
+      {/* Add other components or content here */}
+    </div>
+  );
+};
+
+export default App;
