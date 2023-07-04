b1c8f33f726353f41ea532edcc83a85ed86fbc40
Alex Ocegueda <alexocegueda59@gmail.com>
2023-07-04 00:57:10 -0500

    Added description for animals and added filler dog

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Animal = ({ displayName, link, description, imageSrc }) => {
+const Animal = ({ imgUrl, name, breed, description, eyeColor, playful, longHaired }) => {
     return (
         <Container>
-            <Text>
-                <h2>{displayName}</h2>
-                <Description>{description}</Description>
-                <Link href={link}>See on NHS →</Link>
-            </Text>
-            <Image src={imageSrc} alt="Animal Image" />
+        <Text>
+            <h2>{name}</h2>
+            <Description>
+            <strong>Breed:</strong> {breed}
+            <br />
+            <strong>Eye Color:</strong> {eyeColor}
+            <br />
+            <strong>Playful:</strong> {playful ? 'Yes' : 'No'}
+            <br />
+            <strong>Long-Haired:</strong> {longHaired ? 'Yes' : 'No'}
+            <br />
+            {description}
+            </Description>
+        </Text>
+        <Image src={imgUrl} alt="Animal Image" />
         </Container>
     );
 };
@@ -17,55 +26,50 @@ const Animal = ({ displayName, link, description, imageSrc }) => {
 export default Animal;
 
 const Text = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Image = styled.img`
-    border-radius: 1rem;
-    object-fit: cover;
-    width: 20rem;
+  border-radius: 1rem;
+  object-fit: cover;
+  width: 20rem;
+  margin-right: 1rem;
 
-    @media screen and (max-width: 768px) { /* Medium screens */
-        width: 15rem;
-    }
+  @media screen and (max-width: 768px) {
+    /* Medium screens */
+    width: 15rem;
+  }
 
-    @media screen and (max-width: 576px) { /* Small screens */
-        width: 100%;
-        max-width: 20rem;
-    }
+  @media screen and (max-width: 576px) {
+    /* Small screens */
+    width: 100%;
+    max-width: 20rem;
+  }
 `;
 
 const Container = styled.div`
-    display: flex;
-    width: 40rem;
-    height: 15rem;
-    margin-top: 2rem;
-    padding: 1rem;
+  display: flex;
+  width: 40rem;
+  height: 15rem;
+  margin-top: 2rem;
+  padding: 1rem;
 
-    @media screen and (max-width: 768px) { /* Medium screens */
-        width: 30rem;
-        height: 13rem;
-    }
+  @media screen and (max-width: 768px) {
+    /* Medium screens */
+    width: 30rem;
+    height: 13rem;
+  }
 
-    @media screen and (max-width: 576px) { /* Small screens */
-        width: 100%;
-        flex-direction: column;
-        height: auto;
-    }
-`;
-
-const Tools = styled.span`
-    display: flex;
-    font-weight: semi-bold;
-`;
-
-const Link = styled.a`
-    margin-left: 0.3rem;
-    font-weight: semi-bold;
+  @media screen and (max-width: 576px) {
+    /* Small screens */
+    width: 100%;
+    flex-direction: column;
+    height: auto;
+  }
 `;
 
 const Description = styled.p`
-    color: black;
-    margin-right: .5rem;
+  color: black;
+  margin-right: 0.5rem;
 `;
