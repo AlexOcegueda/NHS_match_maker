d292150d224cce0524a4d7cbba6fb1c23140a22e
Alex Ocegueda <alexocegueda59@gmail.com>
2023-09-25 16:32:39 -0500

    Changed filesystem names

--- old/src/pages/App.js
+++
@@ -1,9 +0,0 @@
-// src/App.js
-import React from 'react';
-import Layout from '../components/Layout';
-
-const App = ({ element }) => {
-  return <Layout>{element}</Layout>;
-};
-
-export default App;
